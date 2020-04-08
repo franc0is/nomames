61c4f15a804bdc637442d82c7ca6961a85e71941
topace14 <craneae@gmail.com>
2020-04-07 20:30:27 -0400

    fixed!

--- src/dice.js
+++ src/dice.js
@@ -58,6 +58,7 @@ export class Dice extends Phaser.GameObjects.Sprite {
         let clickDelay = this.scene.time.now - this.lastClickTime;
         this.lastClickTime = this.scene.time.now;
         if (clickDelay < 350) {
+            this.scene.noMamesButton.setEnabled(false);
             this.roll();
         }
     }
