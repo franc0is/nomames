cf446da160b0e0ce8354ac5d9ee5ce864b02abae
François Baldassari <francois@memfault.io>
2020-04-12 10:58:04 -0700

    Style title, change text a bit

--- src/scenes/first-scene.js
+++ src/scenes/first-scene.js
@@ -9,22 +9,21 @@ export class FirstScene extends Phaser.Scene {
 
     preload() {
         this.load.image('cupanddice','assets/cupanddice.png');
-        this.load.image('title','assets/nomames.png');
+        this.load.image('title','assets/title-glow.png');
     };
 
     create() {
-        this.add.image(360,75, 'title');
-        this.add.image(360,275,'cupanddice');
-        //let text = this.add.text(320,200,'Welcome!',{ color: 'white', fontSize: '20px '});
-        this.add.text(135, 450, 'this no mames game brought to you by the harkness boys',{ color: 'white', fontSize: '14px '});
+        this.add.image(360, 75, 'title');
+        this.add.image(360, 275,'cupanddice');
+        this.add.text(235, 450, 'Brought to you by 400B Studios',{ color: 'white', fontSize: '14px '});
 
-        this.startButton = new TextButton(this, 250, 420, '[ START ]', {
+        this.startButton = new TextButton(this, 250, 420, '[ NEW ]', {
             onClick: () => {
                 this.scene.start('startScene');
             }
         });
         this.add.existing(this.startButton);
-        
+
         this.joinButton = new TextButton(this, 395, 420, '[ JOIN ]', {
             onClick: () => {
                 this.scene.start('joinScene');
